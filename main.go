b02018ab427d0a7de2f81e493c8920e7922fd824
Angel Ruiz <aa09angel@gmail.com>
2025-02-07 21:28:37 -0600

    cors para front

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import (
     "log"
     "github.com/joho/godotenv"
     "github.com/gin-gonic/gin"
-    refrescosInfra "demo/src/refrescos/infraestructure" 
+    "github.com/gin-contrib/cors"
+    "time"
+    refrescosInfra "demo/src/refrescos/infraestructure"
     sabritasInfra "demo/src/sabritas/infraestructure"
 )
 
@@ -16,11 +18,20 @@ func main() {
 
     r := gin.Default()
 
-    refrescosInfra.SetupRoutesRefresco(r)
 
+    r.Use(cors.New(cors.Config{
+        AllowOrigins:     []string{"http://localhost:4200"}, 
+        AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+        AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+        ExposeHeaders:    []string{"Content-Length"},
+        AllowCredentials: true,
+        MaxAge:           12 * time.Hour,
+    }))
+
+    refrescosInfra.SetupRoutesRefresco(r)
     sabritasInfra.SetupRoutesSabrita(r)
 
     if err := r.Run(":8080"); err != nil {
         log.Fatalf("Error al iniciar el servidor: %v", err)
-    }
+    } 
 }
